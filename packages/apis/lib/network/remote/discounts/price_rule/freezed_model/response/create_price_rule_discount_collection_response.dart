b6eba2d7ea94efe74ee6f3c5de7a2a2a01125a62
Nisa <76876477+nisacolak@users.noreply.github.com>
2025-05-06 11:09:57 +0300

    feat: add CreatePriceRuleDiscountSelectedCustomersRequest and update PriceRule model with new fields

--- packages/apis/lib/network/remote/discounts/price_rule/freezed_model/response/create_price_rule_discount_collection_response.dart
+++ packages/apis/lib/network/remote/discounts/price_rule/freezed_model/response/create_price_rule_discount_collection_response.dart
@@ -34,25 +34,26 @@ class PriceRule with _$PriceRule {
     @JsonKey(name: "target_type") String? targetType,
     @JsonKey(name: "target_selection") String? targetSelection,
     @JsonKey(name: "allocation_method") String? allocationMethod,
-    @JsonKey(name: "allocation_limit") dynamic allocationLimit,
+    @JsonKey(name: "allocation_limit") int? allocationLimit,
     @JsonKey(name: "once_per_customer") bool? oncePerCustomer,
     @JsonKey(name: "usage_limit") dynamic usageLimit,
     @JsonKey(name: "starts_at") String? startsAt,
     @JsonKey(name: "ends_at") dynamic endsAt,
     @JsonKey(name: "created_at") String? createdAt,
     @JsonKey(name: "updated_at") String? updatedAt,
-    @JsonKey(name: "entitled_product_ids") List<dynamic>? entitledProductIds,
+    @JsonKey(name: "entitled_product_ids") List<int>? entitledProductIds,
     @JsonKey(name: "entitled_variant_ids") List<dynamic>? entitledVariantIds,
-    @JsonKey(name: "entitled_collection_ids") List<int>? entitledCollectionIds,
+    @JsonKey(name: "entitled_collection_ids")
+    List<dynamic>? entitledCollectionIds,
     @JsonKey(name: "entitled_country_ids") List<dynamic>? entitledCountryIds,
     @JsonKey(name: "prerequisite_product_ids")
     List<dynamic>? prerequisiteProductIds,
     @JsonKey(name: "prerequisite_variant_ids")
     List<dynamic>? prerequisiteVariantIds,
     @JsonKey(name: "prerequisite_collection_ids")
-    List<dynamic>? prerequisiteCollectionIds,
-    @JsonKey(name: "prerequisite_saved_search_ids")
-    List<dynamic>? prerequisiteSavedSearchIds,
+    List<int>? prerequisiteCollectionIds,
+    @JsonKey(name: "customer_segment_prerequisite_ids")
+    List<dynamic>? customerSegmentPrerequisiteIds,
     @JsonKey(name: "prerequisite_customer_ids")
     List<dynamic>? prerequisiteCustomerIds,
     @JsonKey(name: "prerequisite_subtotal_range")
@@ -64,6 +65,8 @@ class PriceRule with _$PriceRule {
     @JsonKey(name: "prerequisite_to_entitlement_quantity_ratio")
     PrerequisiteToEntitlementQuantityRatio?
         prerequisiteToEntitlementQuantityRatio,
+    @JsonKey(name: "prerequisite_to_entitlement_purchase")
+    PrerequisiteToEntitlementPurchase? prerequisiteToEntitlementPurchase,
     @JsonKey(name: "title") String? title,
     @JsonKey(name: "admin_graphql_api_id") String? adminGraphqlApiId,
   }) = _PriceRule;
@@ -73,11 +76,23 @@ class PriceRule with _$PriceRule {
 }
 
 @freezed
+class PrerequisiteToEntitlementPurchase
+    with _$PrerequisiteToEntitlementPurchase {
+  const factory PrerequisiteToEntitlementPurchase({
+    @JsonKey(name: "prerequisite_amount") dynamic prerequisiteAmount,
+  }) = _PrerequisiteToEntitlementPurchase;
+
+  factory PrerequisiteToEntitlementPurchase.fromJson(
+          Map<String, dynamic> json) =>
+      _$PrerequisiteToEntitlementPurchaseFromJson(json);
+}
+
+@freezed
 class PrerequisiteToEntitlementQuantityRatio
     with _$PrerequisiteToEntitlementQuantityRatio {
   const factory PrerequisiteToEntitlementQuantityRatio({
-    @JsonKey(name: "prerequisite_quantity") dynamic prerequisiteQuantity,
-    @JsonKey(name: "entitled_quantity") dynamic entitledQuantity,
+    @JsonKey(name: "prerequisite_quantity") int? prerequisiteQuantity,
+    @JsonKey(name: "entitled_quantity") int? entitledQuantity,
   }) = _PrerequisiteToEntitlementQuantityRatio;
 
   factory PrerequisiteToEntitlementQuantityRatio.fromJson(
