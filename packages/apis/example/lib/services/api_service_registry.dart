edddd5a91628b48a1ec8ec2d4c3a00bb5621fae8
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-05-12 00:24:34 +0300

    add: ListArticlesFromBlogHandler to retrieve articles from a specific blog

--- packages/apis/example/lib/services/api_service_registry.dart
+++ packages/apis/example/lib/services/api_service_registry.dart
@@ -46,6 +46,7 @@ import 'package:example/services/handlers/metafield_handlers/list_metafields_han
 import 'package:example/services/handlers/metafield_handlers/list_metafields_query_parameters_handler.dart';
 import 'package:example/services/handlers/metafield_handlers/update_metafield_handler.dart';
 import 'package:example/services/handlers/online_store_handlers/article/list_all_article_authors_handler.dart';
+import 'package:example/services/handlers/online_store_handlers/article/list_articles_from_blog_handler.dart';
 import 'package:example/services/handlers/online_store_handlers/article/list_most_popular_tags_handler.dart';
 import 'package:example/services/handlers/online_store_handlers/article/list_most_popular_tags_specific_blog_handler.dart';
 import 'package:example/services/handlers/online_store_handlers/article/list_tags_all_articles_handler.dart';
@@ -635,6 +636,15 @@ class ApiServiceRegistry {
       category: ApiCategory.onlineStore,
       subcategory: 'Article',
       handler: ListMostPopularTagsSpecificBlogHandler(),
+    ),
+
+    // 📚 LIST ARTICLES FROM BLOG 
+    ApiService(
+      name: 'List Articles from Blog',
+      endpoint: '/blogs/:blog_id/articles',
+      category: ApiCategory.onlineStore,
+      subcategory: 'Article',
+      handler: ListArticlesFromBlogHandler(),
     ),
     
   ];
