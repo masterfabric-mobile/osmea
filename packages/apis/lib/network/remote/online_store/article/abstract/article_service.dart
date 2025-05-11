b8541f72d55ed45bfc7a6630c3f5919e66802dab
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-05-11 23:38:17 +0300

    add: ListTagsAllArticlesResponse and method to retrieve tags for all articles with optional query parameters

--- packages/apis/lib/network/remote/online_store/article/abstract/article_service.dart
+++ packages/apis/lib/network/remote/online_store/article/abstract/article_service.dart
@@ -1,6 +1,7 @@
 
 import 'package:apis/network/remote/online_store/article/freezed_model/response/list_all_article_authors_response.dart';
 import 'package:apis/network/remote/online_store/article/freezed_model/response/list_most_popular_tags_response.dart';
+import 'package:apis/network/remote/online_store/article/freezed_model/response/list_tags_all_articles_response.dart';
 import 'package:apis/network/remote/online_store/article/freezed_model/response/list_tags_specific_blog_response.dart';
 
 abstract class ArticleService {
@@ -16,6 +17,12 @@ abstract class ArticleService {
   });
 
   Future <ListMostPopularTagsResponse> listMostPopularTags({
+    required String apiVersion,
+    int? limit,
+    bool? popular
+  });
+
+  Future<ListTagsAllArticlesResponse> listTagsAllArticles({
     required String apiVersion,
     int? limit,
     bool? popular
--- packages/apis/lib/network/remote/online_store/article/api/api_article_service.dart
+++ packages/apis/lib/network/remote/online_store/article/api/api_article_service.dart
@@ -4,6 +4,7 @@ import 'package:apis/dio_config/api_dio_client.dart';
 import 'package:apis/network/remote/online_store/article/abstract/article_service.dart';
 import 'package:apis/network/remote/online_store/article/freezed_model/response/list_all_article_authors_response.dart';
 import 'package:apis/network/remote/online_store/article/freezed_model/response/list_most_popular_tags_response.dart';
+import 'package:apis/network/remote/online_store/article/freezed_model/response/list_tags_all_articles_response.dart';
 import 'package:apis/network/remote/online_store/article/freezed_model/response/list_tags_specific_blog_response.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
@@ -40,6 +41,14 @@ abstract class ArticleServiceClient implements ArticleService {
   /// 📦 List most popular tags in the API.
   @GET('/api/{api_version}/articles/tags.json')
   Future<ListMostPopularTagsResponse> listMostPopularTags({
+    @Path('api_version') required String apiVersion,
+    @Query('limit') int? limit,
+    @Query('popular') bool? popular,
+  });
+
+  /// 📦 List tags for all articles in the API.
+  @GET('/api/{api_version}/articles/tags.json')
+  Future<ListTagsAllArticlesResponse> listTagsAllArticles({
     @Path('api_version') required String apiVersion,
     @Query('limit') int? limit,
     @Query('popular') bool? popular,
