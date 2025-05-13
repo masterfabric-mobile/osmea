73bfdb0748c1fc264e9c1786f5457b956ab64a42
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-05-13 16:27:05 +0300

    add: Implement ListAllAssetsTheme feature with request handler for retrieving theme assets

--- packages/apis/example/lib/services/api_service_registry.dart
+++ packages/apis/example/lib/services/api_service_registry.dart
@@ -56,6 +56,7 @@ import 'package:example/services/handlers/online_store_handlers/article/list_mos
 import 'package:example/services/handlers/online_store_handlers/article/list_tags_all_articles_handler.dart';
 import 'package:example/services/handlers/online_store_handlers/article/list_tags_specific_blog_handler.dart';
 import 'package:example/services/handlers/online_store_handlers/article/update_article_handler.dart';
+import 'package:example/services/handlers/online_store_handlers/asset/list_all_assets_theme_handler.dart';
 import 'package:example/services/index.dart';
 import 'handlers/customers_handlers/customer/retrieves_list_of_customers_handler.dart';
 import 'handlers/customers_handlers/customer/retrieves_all_orders_belonging_to_customer_handler.dart';
@@ -695,6 +696,15 @@ class ApiServiceRegistry {
       category: ApiCategory.onlineStore,
       subcategory: 'Article',
       handler: DeleteArticleFromBlogHandler(),
+    ),
+
+    // 🎨 LIST ALL THEME ASSETS
+    ApiService(
+      name: 'List All Theme Assets',
+      endpoint: '/themes/:theme_id/assets',
+      category: ApiCategory.onlineStore,
+      subcategory: 'Asset',
+      handler: ListAllAssetsThemeHandler(),
     ),
     
   ];
