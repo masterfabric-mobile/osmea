5cc1b8d9630539be8919a722889b0aaa5dc5c1a8
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-05-14 14:26:33 +0300

    add: Implement Create Empty Blog With Metafield API request and response models, and service client

--- packages/apis/lib/network/remote/online_store/blog/abstract/blog_service.dart
+++ packages/apis/lib/network/remote/online_store/blog/abstract/blog_service.dart
@@ -1,9 +1,17 @@
 import 'package:apis/network/remote/online_store/blog/freezed_model/request/create_empty_blog_request.dart';
+import 'package:apis/network/remote/online_store/blog/freezed_model/request/create_empty_blog_with_metafield_request.dart';
 import 'package:apis/network/remote/online_store/blog/freezed_model/response/create_empty_blog_response.dart';
+import 'package:apis/network/remote/online_store/blog/freezed_model/response/create_empty_blog_with_metafield_response.dart';
 
 abstract class BlogService {
   Future<CreateEmptyBlogResponse> createEmptyBlog({
     required String apiVersion,
     required CreateEmptyBlogRequest model,
+  });
+
+    /// 📦 Create a new empty blog with a metafield 
+   Future<CreateEmptyBlogWithMetafieldResponse> createEmptyBlogWithMetafield({
+    required String apiVersion,
+    required CreateEmptyBlogWithMetafieldRequest model,
   });
 }
--- packages/apis/lib/network/remote/online_store/blog/api/api_blog_service.dart
+++ packages/apis/lib/network/remote/online_store/blog/api/api_blog_service.dart
@@ -2,7 +2,9 @@ import 'package:apis/apis.dart';
 import 'package:apis/dio_config/api_dio_client.dart';
 import 'package:apis/network/remote/online_store/blog/abstract/blog_service.dart';
 import 'package:apis/network/remote/online_store/blog/freezed_model/request/create_empty_blog_request.dart';
+import 'package:apis/network/remote/online_store/blog/freezed_model/request/create_empty_blog_with_metafield_request.dart';
 import 'package:apis/network/remote/online_store/blog/freezed_model/response/create_empty_blog_response.dart';
+import 'package:apis/network/remote/online_store/blog/freezed_model/response/create_empty_blog_with_metafield_response.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/http.dart';
@@ -25,5 +27,12 @@ abstract class BlogServiceClient implements BlogService {
   Future<CreateEmptyBlogResponse> createEmptyBlog({
     @Path('api_version') required String apiVersion,
     @Body() required CreateEmptyBlogRequest model,
+  });
+
+  /// 📦 Create a new empty blog with a metafield 
+  @POST('/api/{api_version}/blogs.json')
+  Future<CreateEmptyBlogWithMetafieldResponse> createEmptyBlogWithMetafield({
+    @Path('api_version') required String apiVersion,
+    @Body() required CreateEmptyBlogWithMetafieldRequest model,
   });
 }
