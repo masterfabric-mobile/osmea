6b5ae15283fd0ae4858f2cc08369a122e2f13cb3
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-05-15 09:33:57 +0300

    add: Implement Mark Comment Not Spam Restore API handler and response model

--- packages/apis/lib/network/remote/online_store/comment/abstract/comment_service.dart
+++ packages/apis/lib/network/remote/online_store/comment/abstract/comment_service.dart
@@ -1,6 +1,7 @@
 import 'package:apis/network/remote/online_store/comment/freezed_model/request/create_comment_textile_markup_request.dart';
 import 'package:apis/network/remote/online_store/comment/freezed_model/response/approve_and_publish_comment_response.dart';
 import 'package:apis/network/remote/online_store/comment/freezed_model/response/create_comment_textile_markup_response.dart';
+import 'package:apis/network/remote/online_store/comment/freezed_model/response/mark_comment_not_spam_restore_response.dart';
 
 abstract class CommentService {
     /// 📦 Create a comment with textile markup
@@ -11,6 +12,12 @@ abstract class CommentService {
 
     /// 📦 Approve a comment and publish it to the blog
     Future<ApproveAndPublishCommentResponse> approveAndPublishComment({
+      required String apiVersion,
+      required String commentId,
+    });
+
+    /// 📦 Mark a comment as not spam, restoring it to an unapproved or published state
+    Future<MarkCommentNotSpamRestoreResponse> MarkCommentNotSpamRestore({
       required String apiVersion,
       required String commentId,
     });
--- packages/apis/lib/network/remote/online_store/comment/api/api_comment_service.dart
+++ packages/apis/lib/network/remote/online_store/comment/api/api_comment_service.dart
@@ -4,6 +4,7 @@ import 'package:apis/network/remote/online_store/comment/abstract/comment_servic
 import 'package:apis/network/remote/online_store/comment/freezed_model/request/create_comment_textile_markup_request.dart';
 import 'package:apis/network/remote/online_store/comment/freezed_model/response/approve_and_publish_comment_response.dart';
 import 'package:apis/network/remote/online_store/comment/freezed_model/response/create_comment_textile_markup_response.dart';
+import 'package:apis/network/remote/online_store/comment/freezed_model/response/mark_comment_not_spam_restore_response.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/http.dart';
@@ -31,6 +32,13 @@ abstract class CommentServiceClient implements CommentService {
   /// 📦 Approve a comment and publish it to the blog 
   @POST('/api/{api_version}/comments/{comment_id}/approve.json')
   Future<ApproveAndPublishCommentResponse> approveAndPublishComment({
+    @Path('api_version') required String apiVersion,
+    @Path('comment_id') required String commentId,
+  });
+
+  /// 📦 Mark a comment as not spam, restoring it to an unapproved or published
+  @POST('/api/{api_version}/comments/{comment_id}/not_spam.json')
+  Future<MarkCommentNotSpamRestoreResponse> MarkCommentNotSpamRestore({
     @Path('api_version') required String apiVersion,
     @Path('comment_id') required String commentId,
   });
