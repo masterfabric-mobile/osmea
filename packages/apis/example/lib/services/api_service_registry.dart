8207f32706a486387f1390c042e610aec43d952c
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-05-15 09:49:51 +0300

    add: Implement Remove Comment API handler and register in service

--- packages/apis/example/lib/services/api_service_registry.dart
+++ packages/apis/example/lib/services/api_service_registry.dart
@@ -20,6 +20,7 @@ import 'package:example/services/handlers/online_store_handlers/blog/update_exis
 import 'package:example/services/handlers/online_store_handlers/comment/approve_and_publish_comment_handler.dart';
 import 'package:example/services/handlers/online_store_handlers/comment/create_comment_textile_markup_handler.dart';
 import 'package:example/services/handlers/online_store_handlers/comment/mark_comment_not_spam_restore_handler.dart';
+import 'package:example/services/handlers/online_store_handlers/comment/remove_comment_handler.dart';
 import 'package:example/services/handlers/store_properties_handlers/country_handlers/receive_list_of_countries_handler.dart';
 import 'package:example/services/handlers/store_properties_handlers/country_handlers/retrieves_count_of_countries_handler.dart';
 import 'package:example/services/handlers/store_properties_handlers/country_handlers/creates_new_country_handler.dart';
@@ -1052,13 +1053,22 @@ class ApiServiceRegistry {
       handler: ApproveAndPublishCommentHandler(),
     ),
 
-    // 🔄 MARK COMMENT NOT SPAM & RESTORE 
+    // 🔄 MARK COMMENT NOT SPAM & RESTORE
     ApiService(
       name: 'Mark Comment Not Spam & Restore',
       endpoint: '/blogs/:blog_id/articles/:article_id/comments/:comment_id',
       category: ApiCategory.onlineStore,
       subcategory: 'Comment',
       handler: MarkCommentNotSpamRestoreHandler(),
+    ),
+
+    /// 📦 Remove a comment
+    ApiService(
+      name: 'Remove Comment',
+      endpoint: '/blogs/:blog_id/articles/:article_id/comments/:comment_id',
+      category: ApiCategory.onlineStore,
+      subcategory: 'Comment',
+      handler: RemoveCommentHandler(),
     ),
   ];
 
