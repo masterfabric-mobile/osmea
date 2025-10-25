d48ca82057b8458d98a17ceda447c64bb8b3fc7d
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-10-26 01:53:57 +0300

    feat: Refactor MasterViewHydratedCubit to include custom AppBar padding and background color properties for improved layout customization

--- packages/core/lib/src/base/master_view_hydrated_cubit/master_view_hydrated_cubit.dart
+++ packages/core/lib/src/base/master_view_hydrated_cubit/master_view_hydrated_cubit.dart
@@ -2,7 +2,6 @@ library master_view_hydrated_cubit;
 
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
-import 'package:core/src/base/widgets/master_scaffold_widget.dart';
 import 'package:go_router/go_router.dart';
 
 part 'master_view_hydrated_cubit_enums.dart';
@@ -36,8 +35,8 @@ part 'master_view_hydrated_cubit_mixins.dart';
 ///   }
 /// }
 /// ```
-abstract class MasterViewHydratedCubit<V extends BaseViewModelHydratedCubit<S>, S>
-    extends StatelessWidget with MasterViewHydratedCubitMixin {
+abstract class MasterViewHydratedCubit<V extends BaseViewModelHydratedCubit<S>,
+    S> extends StatelessWidget with MasterViewHydratedCubitMixin {
   final Map<String, dynamic> arguments;
   final MasterViewHydratedCubitTypes currentView;
   final Function snackBarFunction;
@@ -47,11 +46,13 @@ abstract class MasterViewHydratedCubit<V extends BaseViewModelHydratedCubit<S>, 
   final Function(String path) goRoute;
   final bool? extendBody;
   final bool? extendBodyBehindAppBar;
+  final Color? backgroundColor;
 
   // Layout configuration - external values
   final SpacerVisibility? navbarSpacer;
   final SpacerVisibility? footerSpacer;
   final PaddingVisibility? horizontalPadding;
+  final AppBarPaddingVisibility? appBarPadding;
   final bool? useSafeArea;
 
   // Spacer types - custom overrides default
@@ -63,6 +64,8 @@ abstract class MasterViewHydratedCubit<V extends BaseViewModelHydratedCubit<S>, 
   // Padding values - custom overrides default
   final double? customHorizontalPadding;
   final double defaultHorizontalPadding;
+  final double? customAppBarPadding;
+  final double defaultAppBarPadding;
 
   final Widget? bottomNavigationBar;
 
@@ -77,9 +80,11 @@ abstract class MasterViewHydratedCubit<V extends BaseViewModelHydratedCubit<S>, 
     this.bottomNavigationBar,
     this.extendBody,
     this.extendBodyBehindAppBar,
+    this.backgroundColor,
     this.navbarSpacer,
     this.footerSpacer,
     this.horizontalPadding,
+    this.appBarPadding,
     this.useSafeArea,
     this.customNavbarSpacerType,
     this.customFooterSpacerType,
@@ -87,6 +92,8 @@ abstract class MasterViewHydratedCubit<V extends BaseViewModelHydratedCubit<S>, 
     this.defaultFooterSpacerType = CoreSpacerType.footer,
     this.customHorizontalPadding,
     this.defaultHorizontalPadding = 16.0,
+    this.customAppBarPadding,
+    this.defaultAppBarPadding = 16.0,
     required this.goRoute,
   }) : assert(arguments.isNotEmpty, 'Arguments must not be empty') {
     FlutterError.onError = (FlutterErrorDetails details) {
@@ -172,9 +179,11 @@ abstract class MasterViewHydratedCubit<V extends BaseViewModelHydratedCubit<S>, 
                 : bottomNavigationBar,
             extendBody: extendBody,
             extendBodyBehindAppBar: extendBodyBehindAppBar,
+            backgroundColor: backgroundColor,
             navbarSpacer: navbarSpacer,
             footerSpacer: footerSpacer,
             horizontalPadding: horizontalPadding,
+            appBarPadding: appBarPadding,
             useSafeArea: useSafeArea,
             customNavbarSpacerType: customNavbarSpacerType,
             customFooterSpacerType: customFooterSpacerType,
@@ -182,6 +191,8 @@ abstract class MasterViewHydratedCubit<V extends BaseViewModelHydratedCubit<S>, 
             defaultFooterSpacerType: defaultFooterSpacerType,
             customHorizontalPadding: customHorizontalPadding,
             defaultHorizontalPadding: defaultHorizontalPadding,
+            customAppBarPadding: customAppBarPadding,
+            defaultAppBarPadding: defaultAppBarPadding,
           );
         },
       );
