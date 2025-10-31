e1406506b3877226aa8b554db290d0f3eecd1098
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-11-01 01:36:49 +0300

    feat: Register ProfileViewModel and AuthDebugViewModel in dependency injection configuration for improved view management

--- projects/storefront_woo/lib/app/core/config/config_di.config.dart
+++ projects/storefront_woo/lib/app/core/config/config_di.config.dart
@@ -11,12 +11,16 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
+import 'package:storefront_woo/app/views/view_auth_debug/models/auth_debug_view_model.dart'
+    as _i539;
 import 'package:storefront_woo/app/views/view_cart/models/cart_view_model.dart'
     as _i892;
 import 'package:storefront_woo/app/views/view_home/models/home_view_model.dart'
     as _i867;
 import 'package:storefront_woo/app/views/view_product_detail/models/product_detail_view_model.dart'
     as _i819;
+import 'package:storefront_woo/app/views/view_profile/models/profile_view_model.dart'
+    as _i386;
 import 'package:storefront_woo/app/views/view_search/models/search_view_model.dart'
     as _i53;
 import 'package:storefront_woo/app/views/view_wishlist/models/wishlist_view_model.dart'
@@ -36,6 +40,8 @@ extension GetItInjectableX on _i174.GetIt {
     );
     gh.factory<_i867.HomeViewModel>(() => _i867.HomeViewModel());
     gh.factory<_i53.SearchViewModel>(() => _i53.SearchViewModel());
+    gh.factory<_i386.ProfileViewModel>(() => _i386.ProfileViewModel());
+    gh.factory<_i539.AuthDebugViewModel>(() => _i539.AuthDebugViewModel());
     return this;
   }
 }
