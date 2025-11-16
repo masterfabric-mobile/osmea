dcf7b5ce17285634d6c000b5e9ab0cea92b7343f
yuceel <108794593+yuceel@users.noreply.github.com>
2025-11-16 22:29:00 +0100

    add: account view exports and update routes to include account navigation

--- packages/core/lib/core.dart
+++ packages/core/lib/core.dart
@@ -174,3 +174,10 @@ export 'package:core/src/views/empty_view/cubit/empty_view_state.dart';
 export 'package:core/src/views/search/search_view.dart';
 export 'package:core/src/views/search/cubit/search_cubit.dart';
 export 'package:core/src/views/search/cubit/search_state.dart';
+
+// 👤 Exports the account view and cubit
+export 'package:core/src/views/account/account_view.dart';
+export 'package:core/src/views/account/cubit/account_cubit.dart';
+export 'package:core/src/views/account/cubit/account_state.dart';
+export 'package:core/src/views/account/widgets/account_widget.dart';
+export 'package:core/src/views/account/widgets/orders_list_widget.dart';
