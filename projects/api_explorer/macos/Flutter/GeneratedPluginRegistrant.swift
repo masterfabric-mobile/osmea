f4d0d68bc233a41cd2896a7adfc698e2837a00b1
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-12-13 01:44:12 +0300

    chore: upgrade get_it dependency to version 8.3.0, add flutter_local_notifications to Podfile.lock, and register it in GeneratedPluginRegistrant.swift

--- projects/api_explorer/macos/Flutter/GeneratedPluginRegistrant.swift
+++ projects/api_explorer/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import firebase_analytics
 import firebase_core
 import firebase_remote_config
 import flutter_inappwebview_macos
+import flutter_local_notifications
 import package_info_plus
 import path_provider_foundation
 import share_plus
@@ -26,6 +27,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
