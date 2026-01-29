05284927a279e39032e3bf6f648dc39fec2f70ce
Melih Alakabak <melihalkbk@gmail.com>
2026-01-30 00:10:06 +0300

    fix: update WordPress site URL in starter.dart to a generic example for improved clarity

--- projects/storefront_woo/lib/starter.dart
+++ projects/storefront_woo/lib/starter.dart
@@ -70,7 +70,7 @@ launchApp({String environment = 'dev'}) async {
     debugPrint('📡 Attempting to load configuration from WordPress...');
 
     wordPressService = WordPressConfigService(
-      baseUrl: 'https://masterfabric.store', // WordPress site URL
+      baseUrl: 'https://example.com', // WordPress site URL
     );
 
     wordPressConfigIntegration = WordPressConfigIntegration(
