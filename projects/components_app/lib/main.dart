8cc311e49054d24fc6038e281518c1cd6c861709
Sultan Kayı <sultankayi01@gmail.com>
2026-02-27 00:27:24 +0300

    feat: Enable path URL strategy for web support in main.dart

--- projects/components_app/lib/main.dart
+++ projects/components_app/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:core/core.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:get_it/get_it.dart';
 import './bootstrap_web.dart';
 import './routes/app_routes.dart';
 import './widgets/device_frame_wrapper.dart';
 
 void main() async {
+  usePathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
     await runBeforeWeb();
