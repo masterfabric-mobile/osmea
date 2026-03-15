ba3031aa3e5b8a12faca9a94605e7b92ce1d9120
Berkay Vuranok <vuranokb@gmail.com>
2026-03-16 01:19:46 +0300

    fix: Change navigation from profile to dashboard in admin products view

--- projects/storefront_supabase/lib/app/views/admin/products/products_view.dart
+++ projects/storefront_supabase/lib/app/views/admin/products/products_view.dart
@@ -32,7 +32,7 @@ class AdminProductsView
             backgroundColor: OsmeaColors.white,
             foregroundColor: OsmeaColors.black,
             leading: OsmeaComponents.iconButton(
-              onPressed: () => goRoute(AdminRoutes.profile),
+              onPressed: () => goRoute(AdminRoutes.dashboard),
               icon: Icon(Icons.arrow_back, color: OsmeaColors.black),
             ),
           ),
